cea33b8f2f037b413398594a23bc08107c27dda5
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-02-18 21:40:03 +0100

    formulaire amélioré (ajout photos), collection dans la catégorie

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -16,7 +16,7 @@ class CastlesController < ApplicationController
     @castle = Castle.new(castle_params)
     @castle.user_id = current_user.id
     if @castle.save
-      redirect_to castle_path(castle.id), notice: "Château créé avec succès."
+      redirect_to castle_path(@castle), notice: "Château créé avec succès."
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,6 +25,6 @@ class CastlesController < ApplicationController
   private
 
   def castle_params
-    params.require(:castle).permit(:name, :category, :address, :description, :price_per_day, :special_feature, photos: [])
+    params.require(:castle).permit(:name, :category, :address, :description, :price_per_day, :special_feature, :photo)
   end
 end
--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,5 +1,8 @@
 class Castle < ApplicationRecord
   belongs_to :user
-
   has_one_attached :photo
+
+  CATEGORIES = ["Manoir", "Hôtel Particulier", "Forteresse", "Pavillon de chasse", "Commanderie", "Palais", "Demeure seigneuriale"]
+
+  validates :category, inclusion: { in: CATEGORIES }
 end
