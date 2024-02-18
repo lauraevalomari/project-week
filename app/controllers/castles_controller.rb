b528788d91d98e9e51ab14f374403eba0d4cbfdc
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-02-18 18:36:36 +0100

    création de la vue new (formulaire)

--- app/controllers/castles_controller.rb
+++ app/controllers/castles_controller.rb
@@ -16,7 +16,7 @@ class CastlesController < ApplicationController
     @castle = Castle.new(castle_params)
     @castle.user_id = current_user.id
     if @castle.save
-      redirect_to castle_path(@castle), notice: "Castle was successfully created."
+      redirect_to castle_path(castle.id), notice: "Château créé avec succès."
     else
       render :new, status: :unprocessable_entity
     end
@@ -25,6 +25,6 @@ class CastlesController < ApplicationController
   private
 
   def castle_params
-    params.require(:castle).permit(:name, :category, :address, :description, :price_per_day, :special_feature)
+    params.require(:castle).permit(:name, :category, :address, :description, :price_per_day, :special_feature, photos: [])
   end
 end
