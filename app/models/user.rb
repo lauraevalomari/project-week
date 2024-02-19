b5dc6e8ba9c74dc1db8a703d0740c5d02f0ddb73
Eleonore Lacombe <eleonore.lac@gmail.com>
2024-02-19 15:22:02 +0100

    fixer erreur has many

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :bookings, :castles
+  has_many :bookings
+  has many :castles
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
