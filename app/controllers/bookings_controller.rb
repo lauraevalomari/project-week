64e84c40284baa17e36a3e3365898fc2bd9c1a4e
Myriam ZAIBET <myriam.zaibet@gmail.com>
2024-02-23 04:18:16 +0100

    ajout des réservations

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,24 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.where(user_id: current_user.id)
+  end
+
   def create
     @castle = Castle.find(params[:castle_id])
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
     @booking.castle = @castle
     if @booking.save
-      redirect_to castle_path(@castle)
+      redirect_to bookings_path
     else
       render "castles/show", status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, status: :see_other
   end
 
   private
